61e607f38050d7d579d709ceae418366c6be1085
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-08-31 07:20:14 +0300

    Add print color code and lock-unlock color

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const cols = document.querySelectorAll('.col');
 
+document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  if (e.code.toLowerCase() === 'space') {
+    setRandomColours();
+  }
+});
+
+document.addEventListener('click', (event) => {
+  const type = event.target.dataset.type;
+
+  if (type === 'lock') {
+    const node =
+      event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0];
+
+    node.classList.toggle('fa-lock-open');
+    node.classList.toggle('fa-lock');
+  }
+});
+
 function generateRandomColor() {
   const hexCodes = '0123456789ABCDEF';
 
@@ -12,8 +33,28 @@ function generateRandomColor() {
 
 function setRandomColours() {
   cols.forEach((col) => {
-    col.style.background = generateRandomColor();
+    const isLocked = col.querySelector('i').classList.contains('fa-lock');
+
+    if (isLocked) {
+      return;
+    }
+
+    const text = col.querySelector('h2');
+    const button = col.querySelector('button');
+    // const color = generateRandomColor();
+    const color = chroma.random();
+
+    text.textContent = color;
+    col.style.background = color;
+
+    setTextColor(text, color);
+    setTextColor(button, color);
   });
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
 setRandomColours();
